ec624d1ccc43e16a6092f2e5c113397fe8b6b786
Steven Bryant <stevelad_83@yahoo.com>
2022-11-28 14:58:36 -0500

    added renderToDo function

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,29 +5,27 @@ const SUPABASE_KEY =
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo) {
-    // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
-    // once you have a response from supabase, comment this back in:
-    // return checkError(response);
+    const response = await client
+        .from('todos')
+        .insert({ todo: todo, complete: false, user_id: client.auth.user().id })
+        .single();
+    return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
